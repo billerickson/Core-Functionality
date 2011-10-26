a2cbc00413946001f271fa9b0e6f3da46bbfccf2
Bill Erickson <bill.erickson@gmail.com>
2011-10-26 16:08:08 -0500

    dont update plugin

--- plugin.php
+++ plugin.php
@@ -43,6 +43,30 @@ include_once( BE_DIR . '/lib/functions/shortcodes.php' );
 // General
 
 /**
+ * Don't Update Plugin
+ * This prevents you being prompted to update if there's a public plugin
+ * with the same name.
+ *
+ * @author Mark Jaquith
+ * @link http://markjaquith.wordpress.com/2009/12/14/excluding-your-plugin-or-theme-from-update-checks/
+ *
+ * @param array $r, request arguments
+ * @param string $url, request url
+ * @return array request arguments
+ */
+function cws_hidden_plugin_12345( $r, $url ) {
+	if ( 0 !== strpos( $url, 'http://api.wordpress.org/plugins/update-check' ) )
+		return $r; // Not a plugin update request. Bail immediately.
+	$plugins = unserialize( $r['body']['plugins'] );
+	unset( $plugins->plugins[ plugin_basename( __FILE__ ) ] );
+	unset( $plugins->active[ array_search( plugin_basename( __FILE__ ), $plugins->active ) ] );
+	$r['body']['plugins'] = serialize( $plugins );
+	return $r;
+}
+add_filter( 'http_request_args', 'cws_hidden_plugin_12345', 5, 2 );
+
+
+/**
  * Remove Menu Items
  *
  * Remove unused menu items by adding them to the array.
