febcda6c50190ddefa8441f955532aec15bfdaaa
Bill Erickson <bill.erickson@gmail.com>
2012-01-25 20:52:50 -0600

    add facebook open graph

--- plugin.php
+++ plugin.php
@@ -37,6 +37,10 @@ define( 'BE_DIR', dirname( __FILE__ ) );
 // Shortcodes
 include_once( BE_DIR . '/lib/functions/shortcodes.php' );
 
+// Facebook Open Graph Tags
+// -- Set default image in general.php
+include_once( BE_DIR . '/lib/functions/facebook.php' );
+
 // Widgets
 //include_once( BE_DIR . '/lib/widgets/widget-social.php' );
 
