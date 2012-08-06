69f1a42f9e083c14e9e941a69ae59091fd7717fd
Bill Erickson <bill.erickson@gmail.com>
2012-08-06 13:44:10 -0500

    Add function for pretty printing

--- lib/functions/general.php
+++ lib/functions/general.php
@@ -112,3 +112,55 @@ function be_default_facebook_image( $meta ) {
 	return $meta;
 }
 //add_filter( 'mfields_open_graph_meta_tags', 'be_default_facebook_image' );
+
+/**
+ * Pretty Printing
+ * 
+ * @author Chris Bratlien
+ *
+ * @param mixed
+ * @return null
+ */
+function be_pp( $obj, $label = '' ) {  
+
+	$data = json_encode(print_r($obj,true));
+    ?>
+    <style type="text/css">
+      #bsdLogger {
+      position: absolute;
+      top: 30px;
+      right: 0px;
+      border-left: 4px solid #bbb;
+      padding: 6px;
+      background: white;
+      color: #444;
+      z-index: 999;
+      font-size: 1.25em;
+      width: 400px;
+      height: 800px;
+      overflow: scroll;
+      }
+    </style>    
+    <script type="text/javascript">
+      var doStuff = function(){
+        var obj = <?php echo $data; ?>;
+        var logger = document.getElementById('bsdLogger');
+        if (!logger) {
+          logger = document.createElement('div');
+          logger.id = 'bsdLogger';
+          document.body.appendChild(logger);
+        }
+        ////console.log(obj);
+        var pre = document.createElement('pre');
+        var h2 = document.createElement('h2');
+        pre.innerHTML = obj;
+ 
+        h2.innerHTML = '<?php echo addslashes($label); ?>';
+        logger.appendChild(h2);
+        logger.appendChild(pre);      
+      };
+      window.addEventListener ("DOMContentLoaded", doStuff, false);
+ 
+    </script>
+    <?php
+}
