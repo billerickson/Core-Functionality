8115539686105a8ae6446bb5bc125c5075fd24f4
Bill Erickson <bill.erickson@gmail.com>
2012-10-09 12:50:16 -0500

    remove shortcodes

--- plugin.php
+++ plugin.php
@@ -28,9 +28,6 @@ define( 'BE_DIR', dirname( __FILE__ ) );
 // Metaboxes
 //include_once( BE_DIR . '/lib/functions/metaboxes.php' );
  
-// Shortcodes
-include_once( BE_DIR . '/lib/functions/shortcodes.php' );
-
 // Facebook Open Graph Tags
 // -- Set default image in general.php
 include_once( BE_DIR . '/lib/functions/facebook.php' );
